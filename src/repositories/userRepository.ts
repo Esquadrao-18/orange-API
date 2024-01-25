6691a2f3a041c694b45f8796a1f7eb70bd3a669d
Glauco Pantoni <glaucopantoni@gmail.com>
2024-01-25 17:40:48 -0300

    feat: adiciona o userRepository

---
+++ src/repositories/userRepository.ts
@@ -0,0 +1,18 @@
+import prisma from '../config/database';
+import {User} from '@prisma/client';
+
+export async function createUser(newUser: SignUpData) {
+    return prisma.user.create({
+        data: newUser,
+    });
+}
+
+export async function findUserByEmail(email: string) {
+    return prisma.user.findUnique({
+        where: {email},
+    });
+}
+
+
+export interface SignUpData extends Omit<User, 'id'> {}
+export interface SignInData extends Pick<User, 'email' | 'hashedPassword'> {}
