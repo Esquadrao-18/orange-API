cd1f6c0a70d94d690cb3d39d00d42d37943c3eb4
Glauco Pantoni <glaucopantoni@gmail.com>
2024-02-04 22:11:41 -0300

    Improve project service and update database schema
    Unused console log statements have been removed from the project service for cleaner and more efficient code. The database schema has been updated to include 'imageUrl' in the 'Project' table and a relationship constraint to delete tags linked to a project when the project is deleted has been added. The method of updating projects in the router has been changed from PUT to PATCH. The status codes for the 'getProjects' and 'getProjectsByUserId' routes have been corrected from 201 to 200.

--- src/repositories/tagRepository.ts
+++ src/repositories/tagRepository.ts
@@ -25,25 +25,6 @@ export async function associateTagWithProject(
     });
 }
 
-export async function deleteProjectTags(projectId: string): Promise<void> {
-    await prisma.projectTag.deleteMany({
-        where: { projectId: projectId },
-    });
-}
-
-export async function getTagsByProjectId(projectId: string): Promise<Tag[]> {
-    const projectTags = await prisma.projectTag.findMany({
-        where: { projectId: projectId },
-    });
-    const tags = await Promise.all(
-        projectTags.map(projectTag => {
-            const findUniqueQuery = { where: { id: projectTag.tagId } };
-            return prisma.tag.findUnique(findUniqueQuery);
-        }),
-    );
-    return tags.filter(tag => tag !== null) as Tag[];
-}
-
 export async function getProjectTags(projectId: string): Promise<ProjectTag[]> {
     return await prisma.projectTag.findMany({
         where: { projectId: projectId },
