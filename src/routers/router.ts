79b7cc823640747131fca655b8d7c408e028d94d
Glauco Pantoni <glaucopantoni@gmail.com>
2024-02-05 01:46:06 -0300

    Add health check route and import necessary modules
    The express Router now includes the Request and Response modules. Additional code was also added for the GET route '/statusMonitor', to check the status of the server and respond with a status of 204.

--- src/routers/router.ts
+++ src/routers/router.ts
@@ -1,9 +1,14 @@
-import { Router } from 'express';
+import { Router, Request, Response } from 'express';
 import userRouter from './userRouter';
 import projectRouter from './projectRouter';
 
 const router = Router();
 router.use(userRouter);
 router.use(projectRouter);
+
+// Health check route
+router.get('/statusMonitor', (req: Request, res: Response) => {
+    res.sendStatus(204);
+});
 
 export default router;
